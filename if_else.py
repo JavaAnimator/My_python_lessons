925f114f03d0d6b283f21ce0bd8d97df8c1ce004
Javohir <janimator007@gmail.com>
2025-02-23 22:57:44 +0500

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -8,3 +8,11 @@ if son >= 10:
     print("Raxmat")
 elif son < 10:
     print("Bu juft son emas")
+#3 task
+yosh = int(input("Yoshingizni kiriting: "))
+if yosh <= 4 or yosh >= 60:
+     print("Sizga kirish bepul ")
+elif yosh <= 18:
+     print("Sizga kirish 10ming so'm")
+elif yosh > 18:
+     print("Sizg kirish 20ming so'm")
