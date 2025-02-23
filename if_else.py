c1dd619e654623fa1318b38b7e84450f43840bd9
Javohir <janimator007@gmail.com>
2025-02-23 22:23:11 +0500

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -2,3 +2,9 @@ n1 = int(input("firts number: "))
 n2 = int(input("second number: "))
 if n1 == n2:
     print("The numbers are same")
+#2 task
+son = int(input("Juft son kiriting: "))
+if son >= 10:
+    print("Raxmat")
+elif son < 10:
+    print("Bu juft son emas")
