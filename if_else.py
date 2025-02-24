9a4c223f4fd84b3dc4cdcf6ff32d1712e29568cb
Javohir <janimator007@gmail.com>
2025-02-24 19:05:28 +0500

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -16,3 +16,41 @@ elif yosh <= 18:
      print("Sizga kirish 10ming so'm")
 elif yosh > 18:
      print("Sizg kirish 20ming so'm")
+#task 4
+
+#menuni list
+menu = ['olma', 'banan', 'tuxum', 'mandarin', 'limon']
+ovqat = input("1 chi ovqatni kiriting: ")
+ovqat2 = input("2 chi ovqatni kiriting: ")
+ovqat3 = input("3 chi ovqatni kiriting: ")
+ovqat4 = input("4 chi ovqatni kiriting: ")
+ovqat5 = input("5 chi ovqatni kiriting: ")
+# har biri uchun alohida if else ko'rsatib o'tildi
+if ovqat.lower() in menu:
+    print(f"Do'konda {ovqat} bor")
+else:
+    print(f"Do'konda {ovqat} yo'q")
+
+# har bir input uchun alohida if else ko'rsatib o'tildi
+if ovqat2.lower() in menu:
+    print(f"Do'konda {ovqat2} bor")
+else:
+    print(f"Do'konda {ovqat2} yo'q")
+
+# har bir input uchun alohida if else ko'rsatib o'tildi
+if ovqat3.lower() in menu:
+    print(f"Do'konda {ovqat3} bor")
+else:
+    print(f"Do'konda {ovqat3} yo'q")
+
+# har bir input uchun alohida if else ko'rsatib o'tildi
+if ovqat4.lower() in menu:
+    print(f"Do'konda {ovqat4} bor")
+else:
+    print(f"Do'konda {ovqat4} yo'q")
+
+# har bir input uchun alohida if else ko'rsatib o'tildi
+if ovqat5.lower() in menu:
+    print(f"Do'konda {ovqat5} bor")
+else:
+    print(f"Do'konda {ovqat5} yo'q")
