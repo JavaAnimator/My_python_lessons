ebeffcf0af0a25931f316c2e731a6ba2b1d63b6e
Javohir <janimator007@gmail.com>
2025-05-13 17:38:26 +0500

    Calculator.py
    Easy calculator in python

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+number1= int(input("Enter Your 1st Number: "))
+action = input("Enter Your Action: ")
+number2 = int(input("Enter Your 2nd Number: "))
+
+if action == "+":
+  print(number1 + number2)
+elif action == "-":
+  print(number1 - number2)
+elif action == "*":
+  print(number1 * number2)
+elif action == "/":
+  print(number1 / number2)
+
