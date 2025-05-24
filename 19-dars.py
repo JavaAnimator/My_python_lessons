eca7f007a3bb2d5d72a921ae9f763d0244b6e71f
Javohir <janimator007@gmail.com>
2025-05-24 15:29:24 +0500

    19-dars.py

---
+++ 19-dars.py
@@ -0,0 +1,42 @@
+# #1m def yosh_hisbola(ism, yil):
+# #     print(f"{ism.title()}ning yoshi {2025 - yil}da")
+    
+    
+# # yosh_hisbola("Javohir", 2007)
+
+# #2m
+# # def kvakub(bson, ison):
+# #     print(f"{bson*ison}")
+    
+# # kvakub(3, 3)
+
+# #3m
+# #qilolmadim
+# # def bol(son):
+# #     for n in range(2,12):
+# #         if not son%n:
+# #             print(f"{son} {n}ga qoldiqsiz bulinadi")
+# # bol(7)
+        
+
+# #4m
+
+# # def xvay(x, y):
+# #     if x>y:
+# #         print(x,">",y)
+# #     elif x<y:
+# #         print(x,"<",y)
+# #     else:
+# #         print(x,"=",y)
+        
+# # xvay(10, 10)
+
+# def juftmi(son):
+#     if son%2:
+#         print("toq")
+#     else:
+#         print(f"{son} juft son")
+        
+        
+# juftmi(77)
+# juftmi(100)
